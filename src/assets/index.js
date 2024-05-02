1ebb3caec715da70c9696e91f3d1cebcb05de07e
Anita <89337095+Alaire1@users.noreply.github.com>
2024-05-02 14:28:10 +0000

    logo of three js is now showing

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,15 +1,11 @@
 import logo from "./logo.svg";
 import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
 import web_icon from "./web_icon.png"
 import software_icon from "./software_icon.png"
 import art_icon from "./art_icon.png"
-import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
 import css from "./tech/css.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
@@ -20,8 +16,6 @@ import tailwind from "./tech/tailwind.png";
 import threejs from "./tech/threejs.svg";
 import c from "./tech/c.png";
 import cpp from "./tech/cpp.png";
-
-
 import PortfolioBook from "./PortfolioBook.png";
 import TelephoneNumberValidator from "./TelephoneNumberValidator.png";
 import Cub3D from "./cub3d.png";
@@ -29,9 +23,6 @@ import Cub3D from "./cub3d.png";
 export {
   logo,
   backend,
-  creator,
-  mobile,
-  web,
   software_icon,
   web_icon,
   art_icon,
